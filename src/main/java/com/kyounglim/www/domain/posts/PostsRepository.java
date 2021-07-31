36d399b81aabbf591dca1649d59c235c544b4456
seo <appsonge@gmail.com>
2021-08-01 02:03:47 +0900

    update

--- src/main/java/com/kyounglim/www/domain/posts/PostsRepository.java
+++ src/main/java/com/kyounglim/www/domain/posts/PostsRepository.java
@@ -2,7 +2,6 @@ package com.kyounglim.www.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.stream.Stream;
 
@@ -14,11 +13,25 @@ import java.util.stream.Stream;
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // Mybatis,ibatis 에서는 DAO 의 역할과 같은 DB Layer 접근자 JpaRepository<Entity클래스, PK타입>를 상속하면 기본적인 CRUD 메소드가 자동생성
 
+    // p <- 별칭
     @Query("SELECT p " +
             "FROM Posts p " +
             "ORDER BY p.id DESC")
+    //SELECT
+    //      p.id as id,
+    //      p.photo as photo,
+    //      p.item as item,
+    //      p.material as material,
+    //      p.stock as stock,
+    //      p.content as content
+    //from
+    //      Posts p
+    //ORDER BY
+    //      p.id DESC
     Stream<Posts> findAllDesc();
 
     void deleteById(Long id);
+
+
 
 }
