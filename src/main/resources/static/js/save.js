7b6226ba321b2a3b798364b770e0bff44cd22b70
seo <appsonge@gmail.com>
2021-08-09 00:55:39 +0900

    update

--- src/main/resources/static/js/save.js
+++ src/main/resources/static/js/save.js
@@ -1,28 +1,32 @@
 var create = {
-    init : function () {
+    init: function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
         });
     },
-    save : function () {
-        var form = $('#form');
+    save: function () {
+        var form = $('#form')[0];
         var data = new FormData(form);
-        alert(form);
-
+        console.log(form)
         $.ajax({
             type: 'POST',
             url: '/save',
-            dataType: 'multipart/form-data',
-            data: data
-        }).done(function() {
-            alert('글이 등록되었습니다.');
-            location.reload();
-        }).fail(function (error) {
-            alert(error);
+            enctype : 'multipart/form-data',
+            data: data,
+            contentType : false,
+            processData : false,
+            success: function (data) {
+                alert('글이 등록되었습니다.');
+                let url = "/post/" + data;
+                location.replace(url);
+            },
+            error: function (error) {
+                alert('에러가 발생했습니다.');
+                alert(error);
+            }
         });
     }
-
 };
 
 create.init();
