2e2296259954b169c78adda29c34f801c66ea9f0
seo <appsonge@gmail.com>
2021-08-26 00:10:38 +0900

    21-08-24 PM 11:02 UPDATE

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,21 @@
-var data = $('#get-data').val(); // 검색한 data
-var globalCurrentPage = $('#get-page').val(); // 현재페이지
+let data = $('#get-data').val(); // 검색한 data
+let currentPage = $('#get-page').val(); // 현재페이지
 let totalData = $('#totaldata').val(); // 게시글 전체 갯수
 let dataPerPage = 10;
 let pageCount = 10;
 let totalPage;
 
 $(document).ready(function () {
+    //페이징 표시 호출
     paging(totalData, dataPerPage, pageCount, 1);
 });
+function paging(totalData, dataPerPage, pageCount, currentPage) {
+    console.log("currentPage : " + currentPage);
 
-function paging(totalData, dataPerPage, pageCoutn, currentPage) {
     totalPage = Math.ceil(totalData / dataPerPage); //총 페이지 수
 
-    if (totalPage < pageCount) {
-        pageCount = totalPage;
+    if(totalPage<pageCount){
+        pageCount=totalPage;
     }
 
     let pageGroup = Math.ceil(currentPage / pageCount); // 페이지 그룹
@@ -30,31 +32,31 @@ function paging(totalData, dataPerPage, pageCoutn, currentPage) {
     let pageHtml = "";
 
     if (prev > 0) {
-        pageHtml += "<li><a href='#' id='prev'> 이전 </a></li>";
+        pageHtml += "<li><a href='/' id='prev'> 이전 </a></li>";
     }
 
     //페이징 번호 표시
     for (var i = first; i <= last; i++) {
         if (currentPage == i) {
             pageHtml +=
-                "<li class='on'><a href='#' id='" + i + "'>" + i + "</a></li>";
+                '<li><a href="/' + (i-1) + '?data=' + data + '">' + i + '</a></li>';
         } else {
-            pageHtml += "<li><a href='#' id='" + i + "'>" + i + "</a></li>";
+            pageHtml += '<li><a href="/' + (i-1) + '?data=' + data + '">' + i + '</a></li>';
         }
     }
 
     if (last < totalPage) {
-        pageHtml += "<li><a href='#' id='next'> 다음 </a></li>";
+        pageHtml += "<li><a href='/' id='next'> 다음 </a></li>";
     }
 
-    $("#pagination").append(pageHtml);
+    $("#pagination").html(pageHtml);
     let displayCount = "";
     displayCount = "현재 1 - " + totalPage + " 페이지 / " + totalData + "건";
     $("#displayCount").text(displayCount);
 
 
     //페이징 번호 클릭 이벤트
-    $("#pagingul li a").click(function () {
+    $("#pagination li a").click(function () {
         let $id = $(this).attr("id");
         selectedPage = $(this).text();
 
@@ -62,19 +64,15 @@ function paging(totalData, dataPerPage, pageCoutn, currentPage) {
         if ($id == "prev") selectedPage = prev;
 
         //전역변수에 선택한 페이지 번호를 담는다...
-        cursor = selectedPage;
+        globalCurrentPage = selectedPage;
         //페이징 표시 재호출
         paging(totalData, dataPerPage, pageCount, selectedPage);
+        //글 목록 표시 재호출
     });
 }
 
-$("#dataPerPage").change(function () {
-    dataPerPage = $("#dataPerPage").val();
-    //전역 변수에 담긴 globalCurrent 값을 이용하여 페이지 이동없이 글 표시개수 변경
-    paging(totalData, dataPerPage, pageCount, globalCurrentPage);
-});
-
 /*
+
 function pageAlgo(bottomSize, cursor){
     let firstBottomNumber = cursor % bottomSize + 1 // 하단 처음
     let lastBottomNumber = cursor % bottomSize + bottomSize; // 하단 마지막
